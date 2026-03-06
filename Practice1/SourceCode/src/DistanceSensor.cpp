6da4633d205ff58c90cc0a68c41440d5e5182338
nicolegs22 <nicole.gomez@ucb.edu.bo>
2026-03-06 17:41:45 -0400

    Carpetas y Estruturas

---
+++ Practice1/SourceCode/src/DistanceSensor.cpp
@@ -0,0 +1,30 @@
+#include "DistanceSensor.h"
+
+DistanceSensor::DistanceSensor(int trig, int echo, float speed, unsigned long time) {
+    trigPin = trig;
+    echoPin = echo;
+    soundSpeed = speed;
+    timeout = time;
+}
+
+void DistanceSensor::begin() {
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+}
+
+float DistanceSensor::medirDistancia() {
+
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+
+    digitalWrite(trigPin, LOW);
+
+    unsigned long duracion = pulseIn(echoPin, HIGH, timeout);
+
+    float distancia = duracion * soundSpeed / 2.0;
+
+    return distancia;
+}
---
+++ Practice1/SourceCode/src/LedController.cpp
@@ -0,0 +1,32 @@
+#include "LedController.h"
+
+LedController::LedController(int red, int blue, int green) {
+    redPin = red;
+    bluePin = blue;
+    greenPin = green;
+}
+
+void LedController::begin() {
+    pinMode(redPin, OUTPUT);
+    pinMode(bluePin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+}
+
+void LedController::actualizar(float distancia) {
+
+    if(distancia < 10) {
+        digitalWrite(redPin, HIGH);
+        digitalWrite(bluePin, LOW);
+        digitalWrite(greenPin, LOW);
+
+    } else if(distancia >= 10 && distancia < 20) {
+        digitalWrite(redPin, LOW);
+        digitalWrite(bluePin, HIGH);
+        digitalWrite(greenPin, LOW);
+
+    } else if(distancia >= 20 && distancia < 30) {
+        digitalWrite(redPin, LOW);
+        digitalWrite(bluePin, LOW);
+        digitalWrite(greenPin, HIGH);
+    }
+}
